7718d9b6145711e7de903dcfa8d327154f2bb83b
gisele ortiz <giseleortizuriel@gmail.com>
2024-04-06 08:54:29 -0300

    tp_clase_31

---
+++ src/middlewares/accessAdmin.js
@@ -0,0 +1,11 @@
+const admins = ['GISELE', 'Greta', 'Vim', 'Tim']
+
+const accessAdmin = (req,res,next) => {
+    if(admins.includes(req.query.username)){
+        return next()
+    }
+    return res.redirect('/no-access')
+
+}
+
+module.exports = accessAdmin
